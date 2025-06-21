b0fcb5666e9333499a7afd022c1c1bbc4e32e19d
Johan Montorfano <me@johanmontorfano.com>
2025-06-21 15:11:02 +0200

    fix: stream reading now works properly

--- src/tcp/stream.rs
+++ src/tcp/stream.rs
@@ -10,29 +10,40 @@ use super::{did::DIDHandler, listener::StreamHandler};
 /// new line is a "Content-Type" header or a DID header, the size is retrieved
 /// and the entire request is read.
 /// TODO: Implement Content-Type lookup
-pub async fn read_expected_size(mut sock: TcpStream) -> String {
+pub async fn read_expected_size(sock: &mut TcpStream) -> String {
     let mut content = String::new();
-    let mut buffer: Vec<u8> = vec![0, 1];
     let mut req_size: usize = 0;
 
     sock.readable().await.unwrap();
     while req_size == 0 {
-        sock.read_exact(&mut buffer).await.unwrap();
+        let mut buffer: Vec<u8> = vec![0; 1];
+        
+        sock.read(&mut buffer).await.unwrap();
         content.push(buffer[0] as char);
-        buffer.clear();
         if content.ends_with("\n") {
             let did_header = DIDHandler::parse_req_header(&content);
 
             if did_header.len() == 5 {
                 req_size = usize::from_str_radix(
-                    did_header.get(4).unwrap(), 10
+                    did_header.get(4).unwrap().trim(), 10
                 ).unwrap();
             }
         }
     }
-    buffer = Vec::with_capacity(req_size - content.len());
-    sock.read_exact(&mut buffer).await.unwrap();
-    content.push_str(&String::from_utf8(buffer).unwrap());
+    
+    // Here, we read the content kilobyte per kilobyte until reaching the size
+    // of the message. This is due to an issue with sock.read not writing to a
+    // vec properly, while working perfectly writing to a slice.
+
+    let mut read_size = content.len();
+    let mut slice = [0; 1024];
+
+    while read_size < req_size {
+        read_size += 1024;
+        sock.readable().await.unwrap();
+        sock.read(&mut slice).await.unwrap();
+        content.push_str(&String::from_utf8(slice.to_vec()).unwrap());
+    }
 
     content
 }
