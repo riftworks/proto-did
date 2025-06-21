5d3f3e0455108351ca46ab4d4f5a486bdf69a4ad
Johan Montorfano <me@johanmontorfano.com>
2025-06-21 15:13:30 +0200

    fix: cli now uses DIDRequest and DIDResponse

--- tests/integration.rs
+++ tests/integration.rs
@@ -1,9 +1,17 @@
-use proto_did::DIDServer;
+use std::{collections::HashMap, env};
+use proto_did::{cli::start_cli, DIDServer};
 
 #[tokio::test]
 async fn test_server() {
-    DIDServer::build()
-        .set_port(5000)
-        .launch()
-        .await;
+    let env_vars = env::vars().collect::<HashMap<String, String>>();
+    let mode = env_vars.get("MODE");
+
+    if mode.is_some() && mode.unwrap() == "cli" {
+        start_cli().await;
+    } else {
+        DIDServer::build()
+            .set_port(5000)
+            .launch()
+            .await;
+    }
 }
